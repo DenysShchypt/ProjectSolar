7bd0d5436dcca77ed47879bfd827b541f45a34a9
DenysShchypt <denysshchyptsovit@gmail.com>
2024-08-16 18:57:37 +0300

    settings for client

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,11 +1,9 @@
-import { useAppDispatch, useAppSelector } from "../utils/hooks";
 
 function App() {
-  const dispatch = useAppDispatch();
-  const userEmail = useAppSelector((state) => state.auth.user.firstName);
+
   return (
     <>
-      <h1 style={{ textAlign: "center" }}>{userEmail}</h1>
+      <h1 style={{ textAlign: "center" }}>Hello team</h1>
     </>
   );
 }
