b1c9eaf46888ad9cb4fda60359b7e6f667ef7025
DenysShchypt <denysshchyptsovit@gmail.com>
2024-08-26 19:50:06 +0300

    remuve bug /logout

--- backend/src/modules/auth/auth.controller.ts
+++ backend/src/modules/auth/auth.controller.ts
@@ -167,7 +167,7 @@ export class AuthController {
       httpOnly: true,
       // secure: true,
       sameSite: 'lax',
-      path: '/login',
+      path: '/',
     });
     res.sendStatus(HttpStatus.OK);
   }
